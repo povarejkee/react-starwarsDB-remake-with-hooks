a2c4e55332074c06852118355ebf86c3f2b9a85e
popovis <povarejkee@yandex.ru>
2020-01-30 22:49:11 +0300

    started routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Header from '../header'
 import RandomPlanet from '../random-planet'
-import PersonsPage from "../persons-page"
+import PersonsPage from '../persons-page'
 
 import './app.css'
-import PlanetsPage from "../planets-page"
-import StarshipsPage from "../starships-page"
+import PlanetsPage from '../planets-page'
+import StarshipsPage from '../starships-page'
 
 export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <Header />
-                <RandomPlanet />
-                <PersonsPage />
-                <PlanetsPage />
-                <StarshipsPage />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <Header />
+        <RandomPlanet />
+        <Switch>
+          <Route exact path="/">
+            <PersonsPage />
+          </Route>
+          <Route path="/planets">
+            <PlanetsPage />
+          </Route>
+          <Route path="/starships">
+            <StarshipsPage />
+          </Route>
+        </Switch>
+      </Router>
+    )
+  }
 }
